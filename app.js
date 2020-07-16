9200df857063acc67880d3f450fc76dd9b7d4e05
Tong Yu <yutongsf@gmail.com>
2020-07-15 17:24:54 -0700

    react-component complete

--- app.js
+++ app.js
@@ -101,41 +101,222 @@
 
 
 // EXAMPLE (5):  STATE:
+// var App = () => (
+//   <div>
+//     <h2>My Todo List</h2>
+//     <TodoList todos={['learn react', 'Maia lecture', 'crush recast.ly', 'lunch']} />
+//   </div>
+// )
 
+// var TodoList = (props) => (
+//   <ul>
+//     {props.todos.map(todo => <TodoListItem todo={todo} />)}
+//   </ul>
+// )
+
+// class TodoListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       done: false
+//     };
+//   }
+//   onListItemClick() {
+//     this.setState({
+//       done: !this.state.done
+//     });
+//   }
+//   render() {
+//     var style = {
+//       textDecoration: this.state.done ? 'bold' : 'none'
+//     }
+//     return (
+//       <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+//     );
+//   }
+// }
+
+
+// ReactDOM.render(<App />, document.getElementById("app"));
+
+
+
+
+// EXAMPLE 6: Making applications interactive with state
+// var App = () => (
+//   <div>
+//     <h2>My Todo List</h2>
+//     <TodoList todos={['learn react', 'Maia lecture', 'crush recast.ly', 'lunch']} />
+//   </div>
+// )
+
+// var TodoList = (props) => (
+//   <ul>
+//     {props.todos.map(todo => <TodoListItem todo={todo} />)}
+//   </ul>
+// )
+
+// class TodoListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       done: false
+//     };
+//   }
+//   onListItemClick() {
+//     this.setState({
+//       done: !this.state.done
+//     });
+//   }
+//   render() {
+//     var style = {
+//       textDecoration: this.state.done ? 'bold' : 'none'
+//     }
+//     return (
+//       <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById("app"));
+
+// REFACTOR GROCERY LIST WITH CLASS COMPONENT:
+// super class:
+// var App = () => (
+//   <div>
+//     <h2>My GroceryList</h2>
+//     <GroceryList items={['stake', 'orange', 'veggies']} />
+//   </div>
+// )
+
+// // Defining:
+// var GroceryList = (props) => {
+//   console.log(props)
+//   return (
+//     < ul >
+//       {props.items.map(item => <GroceryListItem item={item} />)}
+//     </ul >
+//   )
+// }
+// // functional:
+// class GroceryListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <li>{this.props.item}</li>
+//     )
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById('app'))
+
+// class Counter extends React.Component {
+//   // bind method with constructor:
+//   constructor(props) {
+//     super(props);
+//     this.handleAddOne = this.handleAddOne.bind(this);
+//     // setup state for things:
+//     this.state = {
+//       count: 0
+//     }
+//   }
+//   // add a method:
+//   handleAddOne() {
+//     console.log('handleAddOne')
+//   }
+//   // render the view:
+//   render() {
+//     return (
+//       <div>
+//         <h1>Count: {this.state.count}</h1>
+//         <button onClick={this.handleAddOne}>+1</button>
+//         <button>-1</button>
+//         <button>reset</button>
+//       </div>
+//     )
+//   }
+// }
+
+// ReactDOM.render(<Counter />, document.getElementById('app'))
+
+
+// // EXAMPLE 7: ADD EVENT TO COMPONENTS:
+// var App = () => (
+//   <div>
+//     <h2>My Todo List</h2>
+//     <TodoList todos={['learn react', 'Maia lecture', 'crush recast.ly', 'lunch']} />
+//   </div>
+// )
+
+// var TodoList = (props) => (
+//   <ul>
+//     {props.todos.map(todo => <TodoListItem todo={todo} />)}
+//   </ul>
+// )
+
+// class TodoListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       done: false
+//     };
+//   }
+//   onListItemClick() {
+//     this.setState({
+//       done: !this.state.done
+//     });
+//   }
+//   render() {
+//     var style = {
+//       fontWeight: this.state.done ? 'bold' : 'none'
+//     }
+//     return (
+//       <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById("app"));
+
+
+
+// EXAMPLEE 7: REFACTOR ABOEVE:
+
+// super class:
 var App = () => (
   <div>
-    <h2>My Todo List</h2>
-    <TodoList todos={['learn react', 'Maia lecture', 'crush recast.ly', 'lunch']} />
+    <h2>My GroceryList</h2>
+    <GroceryList items={['stake', 'orange', 'veggies']} />
   </div>
 )
 
-var TodoList = (props) => (
-  <ul>
-    {props.todos.map(todo => <TodoListItem todo={todo} />)}
-  </ul>
-)
-
-class TodoListItem extends React.Component {
+// Defining:
+var GroceryList = (props) => {
+  console.log(props)
+  return (
+    < ul >
+      {props.items.map(item => <GroceryListItem item={item} />)}
+    </ul >
+  )
+}
+// functional:
+class GroceryListItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       done: false
-    };
+    }
   }
-  onListItemClick() {
-    this.setState({
-      done: !this.state.done
-    });
+  onMouseClick(el) {
+    console.log(el)
   }
   render() {
-    var style = {
-      textDecoration: this.state.done ? 'bold' : 'none'
-    }
     return (
-      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
-    );
+      <li onClick={this.onMouseClick.bind(this)}>{this.props.item}</li>
+    )
   }
 }
 
-
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'))
